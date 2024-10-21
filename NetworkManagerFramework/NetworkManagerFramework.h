972f3df5695dec1dacb1eaa6d202e3d55337191f
Sphoorti M Patil <sphoortmpatil@gmail.com>
2024-10-22 00:09:44 +0530

    added .gitignore

---
+++ NetworkManagerFramework/NetworkManagerFramework.h
@@ -0,0 +1,18 @@
+//
+//  NetworkManagerFramework.h
+//  NetworkManagerFramework
+//
+//  Created by CrewPlace Enterprise on 22/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkManagerFramework.
+FOUNDATION_EXPORT double NetworkManagerFrameworkVersionNumber;
+
+//! Project version string for NetworkManagerFramework.
+FOUNDATION_EXPORT const unsigned char NetworkManagerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkManagerFramework/PublicHeader.h>
+
+
